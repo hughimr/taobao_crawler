badd9851dc590e10237a922bd55c52791ed4f833
junges521 <junges521@163.com>
2019-12-19 10:18:57 +0800

    添加启动脚本

---
+++ test.bat
@@ -0,0 +1,5 @@
+adb shell am start -n com.jxl.talentscout.version1/com.jxl.talentscout.douyin.service.ServiceActivity
+adb shell am start -n com.xingin.xhs/.XhsLoginActivity
+adb shell am start -n com.taobao.idlefish/com.juxinli.fuckxianyu.MainActivity
+adb shell am start -n com.fake.taobao/com.fake.taobao.ServiceActivity
+
